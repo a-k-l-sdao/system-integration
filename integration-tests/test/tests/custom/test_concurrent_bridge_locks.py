aea108cab95b57ba579dfeeacb0a0352ba5b3123
alejandro corona <alexander.krone@singularitynet.io>
2026-07-30 14:11:22 +0000

    test(node): allow slow bridge contract deployment

--- integration-tests/test/tests/custom/test_concurrent_bridge_locks.py
+++ integration-tests/test/tests/custom/test_concurrent_bridge_locks.py
@@ -75,7 +75,7 @@ def test_concurrent_bridge_locks_exact_accounting(bridge_shard, timeouts) -> Non
         v1,
         "",
         key,
-        timeouts.deploy_inclusion * 3,
+        timeouts.custom(120),
         timeouts.finalization,
         rho_file="resources/bridge-v2.rho",
         phlo_limit=_PHLO_LIMIT,

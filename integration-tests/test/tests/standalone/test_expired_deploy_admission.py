46954fb5cd37654c3742babcd94dcce8d4fd6d21
alejandro corona <alexander.krone@singularitynet.io>
2026-07-30 15:18:43 +0000

    test(node): anchor expiry to admission height

--- integration-tests/test/tests/standalone/test_expired_deploy_admission.py
+++ integration-tests/test/tests/standalone/test_expired_deploy_admission.py
@@ -25,7 +25,7 @@ def test_expired_deploy_rejected_at_admission(provider, timeouts) -> None:
     handle = provider.create_standalone(config)
     node = Node(handle=handle, role=NodeRole.STANDALONE)
     try:
-        height = node.last_finalized_block().blockInfo.blockNumber
+        height = node.get_current_block_number()
         timestamp = int(time.time() * 1000)
         expired = create_deploy_data(
             BOOTSTRAP_ID.private_key(),
